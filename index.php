310a73cf09cff8ae4e9dfbe4137e2b3cf8f8d18c
Patrick Dobler <paddy@hotmail.ch>
2020-10-29 16:52:32 +0100

    Update SDKs, added jquery min, convert to php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                                 <small id="userSIPasswordError" class="form-text text-danger">Please check your password.</small>
                             </div>
                             <button type="button" class="btn btn-outline-primary text-uppercase mb-3" onclick="signIn()">Sign In</button>
-                            <p>Not a member yet? Become a member: <a href="./pages/sign-up.html"><strong class="text-primary text-uppercase showSignUpForm">Sign Up</strong></a></p>
+                            <p>Not a member yet? Become a member: <a href="./pages/sign-up.php"><strong class="text-primary text-uppercase showSignUpForm">Sign Up</strong></a></p>
                         </div>
                     </div>
                 </div>
@@ -49,36 +49,35 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
     <!-- Firebase -->
     
-<script src="https://www.gstatic.com/firebasejs/5.9.4/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase.js"></script>
 <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.3.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
 
 <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.3.0/firebase-analytics.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-analytics.js"></script>
 <!-- Add Firebase products that you want to use -->
-<script src="https://www.gstatic.com/firebasejs/6.1.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-auth.js"></script>
 
-<script src="https://www.gstatic.com/firebasejs/6.1.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-database.js"></script>
 
 
 <script>
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyDtaXNVwWCDxp5mnqvLDFhOw_ngWnubSMU",
-    authDomain: "fir-authproject-2c928.firebaseapp.com",
-    databaseURL: "https://fir-authproject-2c928.firebaseio.com",
-    projectId: "fir-authproject-2c928",
-    storageBucket: "fir-authproject-2c928.appspot.com",
-    messagingSenderId: "807340522328",
-    appId: "1:807340522328:web:63e986c146d09fb117832d",
-    measurementId: "G-740FSXFRBQ"
+    apiKey: "AIzaSyBGTi4oZxunbqbx7K2LlEU1mnDAAlVSKhQ",
+    authDomain: "ft-test-63e13.firebaseapp.com",
+    databaseURL: "https://ft-test-63e13.firebaseio.com",
+    projectId: "ft-test-63e13",
+    storageBucket: "ft-test-63e13.appspot.com",
+    messagingSenderId: "210338473553",
+    appId: "1:210338473553:web:2ad3f75d83d485ce8afa70"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
